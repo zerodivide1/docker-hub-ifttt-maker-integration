1a1ad2a27a0caaf7219b4e5a0b68e6f011da6b23
Sean Payne <seantpayne@gmail.com>
2017-01-04 15:43:34 -0800

    Added curl command to POST payload

--- ifttt-maker-post.sh
+++ ifttt-maker-post.sh
@@ -8,3 +8,5 @@
 POST_URL=https://maker.ifttt.com/trigger/$1/with/key/$IFTTT_MAKER_KEY
 
 POST_PAYLOAD=$(echo "$2" | jq '{"value1": .repository.repo_name}')
+
+curl -X POST -H "Content-Type: application/json" -d "$POST_PAYLOAD" $POST_URL
